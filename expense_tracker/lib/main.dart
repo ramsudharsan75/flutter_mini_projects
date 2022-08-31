056446cb95e616a9aec656e27c8f7972621f7a76
Ramsudharsan Manoharan <Ramsudharsan.Manoharan@praesidiad.com>
2022-09-01 00:17:33 +0200

    Finalizing Expense tracker

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,16 +1,112 @@
 import 'package:flutter/material.dart';
 
-import './my_app.dart';
+import './models/transaction.dart';
+import './/widgets/new_transaction.dart';
+import './/widgets/transaction_list.dart';
+import './widgets/chart.dart';
 
 void main(List<String> args) {
-  runApp(const AppWrapper());
+  runApp(const MyHomePage());
 }
 
-class AppWrapper extends StatelessWidget {
-  const AppWrapper({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  // String? titleInput;
+  // String? amountInput;
+
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [];
+
+  List<Transaction> get _recentTransactions {
+    var afterDate = DateTime.now().subtract(const Duration(days: 7));
+
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(afterDate);
+    }).toList();
+  }
+
+  void _addNewTransaction(String title, double amount, DateTime chosenDate) {
+    final newTx = Transaction(
+      id: DateTime.now().toString(),
+      title: title,
+      amount: amount,
+      date: chosenDate,
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return NewTransaction(_addNewTransaction);
+      },
+    );
+  }
+
+  void _deleteTransaction(String id) {
+    setState(() {
+      _userTransactions.removeWhere((tx) => tx.id == id);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MyHomePage());
+    return MaterialApp(
+      title: 'Personal Expenses',
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.amber,
+          ),
+          fontFamily: 'Quicksand',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                button: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+          appBarTheme: const AppBarTheme(
+              titleTextStyle: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold))),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Personal Expenses',
+          ),
+          actions: [
+            IconButton(
+              onPressed: () => _startAddNewTransaction(context),
+              icon: const Icon(Icons.add),
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Chart(_recentTransactions),
+              TransactionList(_userTransactions, _deleteTransaction),
+            ],
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: Builder(builder: (context) {
+          return FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context),
+          );
+        }),
+      ),
+    );
   }
 }
